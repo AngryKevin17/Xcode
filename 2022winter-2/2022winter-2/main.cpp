75d60da146661c472fe785a0e28adf1b2c96f958
AngryKevin17 <88230484+AngryKevin17@users.noreply.github.com>
2022-01-16 15:41:26 +0800

    Add files via upload

---
+++ 2022winter-2/2022winter-2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2022winter-2
+//
+//  Created by 陈蓬辉 on 2022/1/16.
+//
+
+#include <iostream>
+
+int main(int argc, const char *argv[])
+{
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
